729fa867897756f363b4757e1c824099c47b2492
chandana M <chandu11288@gmail.com>
2018-02-14 19:21:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
 def prime?(num)
   is_prime = true
+  if num < 0
+    return is_prime = false
+  else
   for i in 2..num-1 do
     if num%i===0
       is_prime = false
@@ -8,5 +11,6 @@ def prime?(num)
       is_prime = true
     end
   end
+end
   is_prime
 end
